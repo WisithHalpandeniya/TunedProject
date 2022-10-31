495bc2a5bae8ad82d5d5cf540d1d7215cf7e4f37
Wisith Halpandeniya <83750757+WisithHalpandeniya@users.noreply.github.com>
2022-11-01 09:15:42 +1100

    Delete api directory

--- api/curlrequest.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:3000 -H 'Content-Type: application/json' -d '{ "payload": {"event": { "name": "new_terms.added" }, "project": { "id": 567931, "name": "Yoonee_project", "public": 0, "open": 0, "created": "2022-05-13T00:24:37+0000" } } }'
-curl -X POST https://api.poeditor.com/v2/projects/export \
-     -d api_token="13a47685705000121cfeb38aabe4b24d" \
-     -d id="548715" \
-     -d language="en" \
-     -d type="json"
